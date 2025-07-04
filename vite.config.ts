834f38aebf34f647a35be469e6a569b4f717d4bf
gtkacz <gmtkacz@proton.me>
2025-07-04 20:10:20 -0300

    sccs fix

--- vite.config.ts
+++ vite.config.ts
@@ -19,7 +19,7 @@ export default defineConfig({
   css: {
     preprocessorOptions: {
       scss: {
-        additionalData: `@import "@/styles/variables.scss";`,
+        additionalData: `@use "@/styles/variables.scss" as *;`,
       },
     },
   },
