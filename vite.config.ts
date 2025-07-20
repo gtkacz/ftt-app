bf9b6ad43db5990bc42f9e27b19ce7b0feb1b18c
gtkacz <gmtkacz@proton.me>
2025-07-20 02:47:27 -0300

    refactor(build): simplify build process and remove forcebuild script

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,12 @@
 import vue from "@vitejs/plugin-vue";
+import { dirname, resolve } from "node:path";
+import { fileURLToPath } from "node:url";
 import { defineConfig } from "vite";
 import viteCompression from "vite-plugin-compression";
 import { VitePWA } from "vite-plugin-pwa";
 import vuetify from "vite-plugin-vuetify";
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -28,6 +32,14 @@ export default defineConfig({
       scss: {
         api: "modern-compiler",
         additionalData: `@use "@/styles/variables.scss" as *;`,
+      },
+    },
+  },
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, "index.html"),
+        nested: resolve(__dirname, "404.html"),
       },
     },
   },
