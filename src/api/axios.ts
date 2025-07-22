df889ef5b11f8b846fcbd6d6428d0c9e56de5f13
gtkacz <gmtkacz@proton.me>
2025-07-21 21:25:47 -0300

    refactor(api/axios.ts): remove axios-auth-refresh and implement a simpler 401 handling using Vue Router.

--- src/api/axios.ts
+++ src/api/axios.ts
@@ -1,6 +1,6 @@
 import axios, { AxiosError, InternalAxiosRequestConfig } from "axios";
-import createAuthRefreshInterceptor from "axios-auth-refresh";
 import { useAuthStore } from "@/stores/auth.ts";
+import { useRouter } from "vue-router";
 
 const api = axios.create({
   baseURL: import.meta.env.VITE_API_URL + "/api",
@@ -20,40 +20,21 @@ api.interceptors.request.use((cfg: InternalAxiosRequestConfig) => {
   return cfg;
 });
 
-// handle 401 + refresh + retry
-createAuthRefreshInterceptor(
-  api,
-  async (failedRequest) => {
-    const auth = useAuthStore();
-    await auth.refreshAccessToken();
-    failedRequest.response!.config.headers![
-      "Authorization"
-    ] = `Bearer ${auth.accessToken}`;
-    return Promise.resolve();
-  },
-  {
-    // only attempt once per request
-    statusCodes: [401],
-  }
-);
-
 api.interceptors.response.use(
   (response) => response,
   async (error: AxiosError) => {
     const status = error.response?.status;
-    const authStore = useAuthStore();
 
-    if (status !== 401 || (error.config as any)?._retry) {
-      return Promise.reject(error);
-    }
+    if (status === 401) {
+      const authStore = useAuthStore();
+      const router = useRouter();
 
-    if (authStore.isAuthenticated) {
       authStore.logout();
-      setTimeout(() => {
-        window.location.href = `/login?redirect=${encodeURIComponent(
-          window.location.pathname
-        )}`;
-      }, 0);
+
+      router.push({
+        path: "/login",
+        query: { redirect: router.currentRoute.value.fullPath },
+      });
     }
 
     return Promise.reject(error);
